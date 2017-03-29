a784b4d347e86264f5c29b5dc5223282694113f8
fenoy96 <jd-xd@hotmail.com>
2017-03-29 20:51:33 +0200

    Refactorización de método de salida

--- factorial.java
+++ factorial.java
@@ -1,36 +1,37 @@
+import java.math.BigInteger;
 import java.util.Scanner;
 
 public class factorial {
 	
-public static long fact(long num) {
-		if (num<=1) {
-			return 1;
+	public static BigInteger fact(BigInteger num) {
+		if (num.compareTo(BigInteger.ONE)<=0) {
+			return BigInteger.ONE;
 		} else {
-			return num*fact(num-1);
+			return num.multiply(fact(num.subtract(BigInteger.ONE)));
 		}
 	}
-
-public static long getNumber() {
+	
+	public static BigInteger getNumber() {
 		Scanner scanInput = new Scanner(System.in);
 		System.out.print("Introduzca un numero: ");
-		long num = scanInput.nextLong();
+		BigInteger num = BigInteger.valueOf(scanInput.nextLong());
 		scanInput.close();
 		return num;
 	}
 	
-	public static void showFac(long num) {
-		if (num<0) {
+	public static void showFac(BigInteger num) {
+		if (num.compareTo(BigInteger.ZERO)<0) {
 			System.out.println("El factorial no esta definido" + 
 " para numeros negativos");
 		} else {
-			long fac = fact(num);
+			BigInteger fac = fact(num);
 			
 			System.out.println("El factorial de "+num+" es "+fac);
 		}
 	}
 	
 	public static void main(String []args) {
-		long num = getNumber();
+		BigInteger num = getNumber();
 		
 		showFac(num);
 	}
