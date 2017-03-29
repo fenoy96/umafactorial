b4cde52abd202cc2fce44e7a8b46975704ec4080
fenoy96 <jd-xd@hotmail.com>
2017-03-29 20:24:12 +0200

    refactorización del metodo de salida

--- factorial.java
+++ factorial.java
@@ -18,9 +18,7 @@ public static long getNumber() {
 		return num;
 	}
 	
-	public static void main(String []args) {
-		long num = getNumber();
-		
+	public static void showFac(long num) {
 		if (num<0) {
 			System.out.println("El factorial no esta definido" + 
 " para numeros negativos");
@@ -29,6 +27,12 @@ public static long getNumber() {
 			
 			System.out.println("El factorial de "+num+" es "+fac);
 		}
+	}
+	
+	public static void main(String []args) {
+		long num = getNumber();
+		
+		showFac(num);
 	}
 
 }
